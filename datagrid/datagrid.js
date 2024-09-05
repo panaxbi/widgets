fc08e0f9c8c2735ca9ccac5113f202fba80b67ba
Uriel Gomez <uriel@panax.io>
2024-09-04 23:09:06 -0600

    * xover.listener.on(`beforeFetch?request`) - method that retrieves selection from navbar controls

--- datagrid/datagrid.js
+++ datagrid/datagrid.js
@@ -333,3 +333,31 @@ xover.listener.on('click::.datagrid .sortable', function () {
 	sortRows.call(this, this.closest('td,th'))
 })
 
+xover.listener.on(`beforeFetch?request`, function ({ request, searchParams }) {
+	if (request.url.hash !== xover.site.seed) return;
+	let getNodeValue = function (node) {
+		if (node.localName == 'selected' && node.namespaceURI == 'http://panax.io/state') {
+			let selection = node.parentNode.single(`*[@id=${node.value}]`) || top.document.createElement('p');
+			return selection.getAttributeNode("id") || selection.getAttributeNode("key") || selection.getAttributeNode("desc")
+		} else {
+			return node.value
+		}
+	}
+	let getNodeName = function (node) {
+		if (node.localName == 'selected' && node.namespaceURI == 'http://panax.io/state') {
+			return node.parentNode.nodeName
+		} else {
+			return node.name.replace(/^state:/, '')
+		}
+	}
+	for (let param of document.querySelectorAll('nav fieldset [xo-slot^=state]')) {
+		let scope = param.scope;
+		let value = getNodeValue(scope) || null;
+		let param_name = '@' + getNodeName(scope);
+		if (value) {
+			searchParams.set(param_name, value)
+		} else {
+			searchParams.delete(param_name)
+		}
+	}
+})

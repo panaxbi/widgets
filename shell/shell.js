a017010ed23a4075b9291b33d8817662d11b143e
Uriel Gomez <uriel@panax.io>
2024-09-06 17:32:31 -0600

    * shell.js print listener:   - Removed comments   - added async and delays

--- shell/shell.js
+++ shell/shell.js
@@ -1,25 +1,10 @@
-xover.listener.on('print', function () {
+xover.listener.on('print', async function () {
+    event.preventDefault();
     let target = this instanceof Node && !(this instanceof Document) && this || document.querySelector('main > article') || document.querySelector('main') || document.body;
-    //let iframes = document.querySelector('main iframe');
-
-    //if (iframes) {
-    //    for (let f = 0; f < iframes.length; ++f) {
-    //        let iframe = iframes[f];
-    //        if (iframe.classList.contains("non-printable")) {
-    //            continue;
-    //        }
-    //        iframe.contentWindow.focus();
-    //        iframe.contentWindow.print();
-    //        f = iframes.length;
-    //    }
-    //} else {
     let content = target.cloneNode(true);
         content.style.width = "100%";
         content.style.height = "100%";
         content.style.overflow = 'visible'
-        //document.body.replaceWith(content);
-        //window.print()
-        //document.body.replaceWith(document_copy);
 
 
 
@@ -43,66 +28,21 @@ xover.listener.on('print', function () {
 
         let printWindow = window.open('', '', 'width=1000,height=800');
 
-        // Write the content to the new window
         printWindow.document.write('<html><head><title>Print Content</title>');
-        /*printWindow.document.write('<style>body { display: none; } #' + divId + ' { display: block; }</style>');*/
         printWindow.document.write('</head><body>');
         printWindow.document.write('</body></html>');
         printWindow.document.body.appendChild(content)
         printWindow.document.body.style.padding = '20px';
-        /*printWindow.document.body.style.border = '1px solid #000';*/
 
+        await xover.delay(200);
         // Close the document to ensure all content is written
-        printWindow.document.close();
+         printWindow.document.close();
 
         copyStyles(document, printWindow.document);
 
-        // Wait for the content to load and then print
         printWindow.onload = function () {
             printWindow.focus(); // Required for some browsers
             printWindow.print();
             printWindow.close();
         };
-
-
-        //    const blob = new Blob([content], { type: 'text/html' });
-        //    const blobUrl = URL.createObjectURL(blob);
-        //    const iframe = document.createElement('iframe');
-        //    iframe.src = blobUrl;
-        //    //iframe.style.position = 'absolute';
-        //    //iframe.style.top = '0';
-        //    //iframe.style.left = '0';
-        //    iframe.style.width = '100vh';
-        //    iframe.style.height = '100vw';
-        //    iframe.style.border = 'none';
-        //    iframe.onload = function () {
-        //        [...iframe.contentDocument.querySelectorAll("[src]")].forEach(img => img.src = xover.URL(img.src).toString());
-        //        const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
-        //        copyStyles(document, iframeDoc);
-
-        //        iframeDoc.documentElement.style.overflow = 'visible';
-        //        iframeDoc.body.style.overflow = 'visible';
-        //        iframe.style.width = "100%"
-        //        iframe.style.height = "100%"
-        //        iframeDoc.querySelector(`main`).style.width = "100%"
-        //        iframeDoc.querySelector(`main`).style.height = "100%"
-
-        //        const height = iframeDoc.documentElement.scrollHeight;
-        //        const width = iframeDoc.documentElement.scrollWidth;
-
-        //        iframe.style.height = height + 'px';
-        //        iframe.style.width = width + 'px';
-
-        //        setTimeout(() => {
-        //            iframe.contentWindow.focus();
-        //            iframe.contentWindow.print();
-        //        }, 500);
-        //    }
-        //    dialog = xover.dom.createDialog(iframe);
-        //    [...dialog.querySelectorAll('dialog > * > menu')].remove()
-        //    dialog.style.height = "100vh"
-        //    dialog.style.width = "100vw"
-        //    dialog.style.overflow = 'visible'
-        event.preventDefault();
-    //}
 })
